72bc2d2985477ef114103f336666518393619558
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:10:23 +0700

    Program memasukan teks

---
+++ src/pbo2/pkg10116374/latihan9/teks/PBO210116374Latihan9Teks.java
@@ -0,0 +1,21 @@
+package pbo2.pkg10116374.latihan9.teks;
+
+
+public class PBO210116374Latihan9Teks {
+
+    /**
+ * @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk memasukan teks
+ */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char" + tab + "berisi " + c);
+    }
+    
+}
